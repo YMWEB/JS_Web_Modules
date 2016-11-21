acd1238cfff0dafbb08667a0e9fac3e4d0cbd053
YUM <yum@glodon.com>
2016-11-21 16:11:29 +0800

    change

--- js_email_subandPub.js
+++ js_email_subandPub.js
@@ -1,11 +1,40 @@
 /*this is the pub/sub*/
 
-var topics = [];
-var sub = function(topic,fun){
-	if(topicshas topic){
-		topics[topic].
+var EventBus = {
+	topics:{};
+	subscribe:function(topic,listener){
+		if(!this.topics[topic])
+			this.topics[topic]=[];
+			this.topics[topic].push(listener);
+	},
+	publish:function(topic,data){
+		if(!this.topics[topic]||this.topics[topic].length<1)return;
+		this.topics[topic].forEach(function(listener){
+			listener(data||{});
+		})
 	}
 }
-var pub = function(topic,fun){
-	
+
+var Mailer = function(){
+	EventBus.subscribe('order/new',this.sendPurchaseEmail);
 }
+
+Mailer.prototype = {
+	sendPurchaseEmail:function(userEmail){
+		console.log('sent email to '+ userEmail)
+	}
+}
+
+var Order = function(params){
+	this.params = params;
+}
+
+Order.prototype = {
+	saveOrder:function(){
+		EventBus.publish('order/new',this.params.userEmail);
+	}
+}
+
+var mail = new Mailer();
+var order = new Order({userEmail:'test@test.com'});
+order.saveOrder();
